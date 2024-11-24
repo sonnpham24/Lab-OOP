453fdba94f74fb1ca333ed68a585eea47c03cf28
SonPham <phamcongson297@gmail.com>
2024-11-24 17:55:11 +0700

    bai6.1

--- OOP-Lab03-20205220/AimsProject/src/aimsprojectPackage/Cart.java
+++ OOP-Lab03-20205220/AimsProject/src/aimsprojectPackage/Cart.java
@@ -85,4 +85,15 @@ public class Cart {
         }
         return total;
     }
+    
+    //Method to print cart
+    public void print() {
+    	System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:");
+        for (int i = 0; i < qtyOrdered; i++) {
+            System.out.println((i + 1) + ". " + itemsOrdered[i].toString());
+        }
+        System.out.println("Total cost: " + totalCost() + " $");
+        System.out.println("***************************************************");
+    }
 }
