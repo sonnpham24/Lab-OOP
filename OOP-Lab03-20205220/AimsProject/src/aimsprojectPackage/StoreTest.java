46bc325d5ce59930632911656c231ee8631c8221
SonPham <phamcongson297@gmail.com>
2024-11-26 12:00:29 +0700

    bai07

---
+++ OOP-Lab03-20205220/AimsProject/src/aimsprojectPackage/StoreTest.java
@@ -0,0 +1,28 @@
+package aimsprojectPackage;
+
+public class StoreTest {
+
+	public static void main(String[] args) {
+		Store store = new Store();
+
+        // Create DVDs
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 124, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", 18.99f);
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Inception", "Science Fiction", "Christopher Nolan", 148, 15.50f);
+
+        // Add DVDs to the store
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+        store.addDVD(dvd3);
+        store.printStore();
+
+        // Remove a DVD from the store
+        store.removeDVD(dvd2);
+        store.printStore();
+
+        // Try removing a DVD that doesn't exist
+        store.removeDVD(dvd4);
+        store.printStore();
+    }
+}
